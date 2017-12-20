365c5afc9a28527dc0f05557008cec564f4137a2
XuHao <504105930@qq.com>
2017-12-21 00:01:13 +0800

    Update MainActivity.kt

--- app/src/main/java/com/hazz/kotlinmvp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/hazz/kotlinmvp/ui/activity/MainActivity.kt
@@ -84,8 +84,6 @@ class MainActivity : BaseActivity() {
     private fun switchFragment(position: Int) {
         val transaction = supportFragmentManager.beginTransaction()
         hideFragments(transaction)
-        showToast(supportFragmentManager.fragments.size.toString())
-        /*重复进入退出视频详情页会导致fragment不断增加*/
         when (position) {
             0 //首页
             -> if (mHomeFragment == null) {
